ff6585146604a0e849dd05e9f3d8678186aa0d61
HerenYU <85064022+HerenYU2007@users.noreply.github.com>
2026-08-02 22:35:15 +0800

    Update scripts/boot.lua

--- scripts/boot.lua
+++ scripts/boot.lua
@@ -1,22 +1,63 @@
-local basalt = require("basalt")
+local gui =
+    require("gui")
 
-local frame = basalt.getMainFrame()
-frame:setBackground(basalt.rgb("#1e1e2e"))
 
-local label = frame:addLabel({
-    x = 2,
-    y = 2,
-    text = "Hello World",
+gui.init()
+
+
+gui.log(
+"System start"
+)
+
+
+gui.setState({
+
+    yaw=90,
+
+    altitude=120,
+
+    engineL=32,
+
+    engineR=32,
+
+    status="Flying"
+
 })
 
-frame:addButton({
-    x = 2,
-    y = 4,
-    text = "Click me",
-})
-    :onClick(function(self)
-        self.text = "Thanks!"
-        label.text = "Button clicked!"
-    end)
 
-basalt.run()
+
+while true do
+
+
+    local e,b,x,y =
+        os.pullEvent(
+            "mouse_click"
+        )
+
+
+    local cmd =
+        gui.click(
+            x,
+            y
+        )
+
+
+    if cmd then
+
+        gui.log(
+            "CMD "..cmd
+        )
+
+
+        -- 这里调用net.lua
+
+        -- net.request(
+        -- server,
+        -- "turn",
+        -- cmd
+        -- )
+
+    end
+
+
+end
